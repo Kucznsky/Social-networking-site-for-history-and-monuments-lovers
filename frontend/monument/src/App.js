25d3ab5d36dbf78611bc4c47fa483868c99b3685
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2022-05-29 18:36:27 +0200

    routing

--- frontend/monument/src/App.js
+++ frontend/monument/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as  Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import HomePage from './components/home_page/HomePage';
+import AddPost from './components/add_post/AddPost';
+import UserPosts from './components/user_posts/UserPosts';
+import MonumentPage from './components/monument_page/MonumentPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <Router>
+      <Navbar></Navbar>
+      <Routes>
+        <Route exact path='/' element={<HomePage/>}>
+        </Route>
+        <Route exact path='/add_post' element={<AddPost/>}>
+        </Route>
+        <Route exact path='/user_posts' element={<UserPosts/>}>
+        </Route>
+        <Route exact path='/monument_page' element={<MonumentPage/>}> 
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/monument/src/components/monument_page/MonumentPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MonumentPage = () => {
+  return (
+    <div>MonumentPage</div>
+  )
+}
+
+export default MonumentPage
