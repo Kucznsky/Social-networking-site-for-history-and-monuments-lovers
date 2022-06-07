6b7296fa0a6a4a0c6375ac576d0ef7f932d030d1
Kucznsky <michal.kuczynski2000@wp.pl>
2022-06-07 18:45:52 +0200

    something in css

--- frontend/monument/src/components/navbar/Navbar.js
+++ frontend/monument/src/components/navbar/Navbar.js
@@ -1,6 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
-    return(<div><input></input></div>);
+  return (
+    <div className="navbar">
+        <nav className="navbar_links">
+          <ul>
+            <li>
+              <Link className='link' to='/'>Home</Link>
+            </li>
+            <li>
+              <Link className='link' to='/my_files'>Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
